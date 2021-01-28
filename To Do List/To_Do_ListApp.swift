94b5a33f77dc3e73d099e71120db609c8e544ac7
Declan Smith <dmansmith03@yahoo.com>
2021-01-28 10:30:26 -0600

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Declan Smith on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
